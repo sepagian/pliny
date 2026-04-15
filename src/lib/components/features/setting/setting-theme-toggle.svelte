87901b30b23312479275f375b2908d4c8693299e
Rizky Dana <rizkydana14@gmail.com>
2026-04-15 21:49:34 +0800

    refactor(setting): migrate theme toggle to bits-ui and lucide icons

--- src/lib/components/features/setting/setting-theme-toggle.svelte
+++ src/lib/components/features/setting/setting-theme-toggle.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
+  import { SunMoon } from "@lucide/svelte";
+  import { Button } from "bits-ui";
   import { toggleMode } from "mode-watcher";
-  import { Button } from "$lib/components/ui/button/index.js";
 </script>
 
-<Button onclick={toggleMode} variant="outline" size="icon">
-  <div
-    class="i-lucide:sun h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90"
-  ></div>
-  <div
-    class="i-lucide:moon absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 !transition-all dark:scale-100 dark:rotate-0"
-  ></div>
+<Button.Root class="btn-secondary p-3" onclick={toggleMode}>
+  <SunMoon size="16" />
   <span class="sr-only">Toggle theme</span>
-</Button>
+</Button.Root>
